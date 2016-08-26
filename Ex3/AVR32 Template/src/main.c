c78949877755297c86770e863dfc61b362fe9ad0
helgeanl <helgelang@gmail.com>
2016-08-27 00:13:54 +0200

    Initial

---
+++ Ex3/AVR32 Template/src/main.c
@@ -0,0 +1,81 @@
+/**
+ * AVR32 UC3 template for TTK4147
+ */
+
+// include header files for all drivers that have been imported from AVR Software Framework (ASF).
+#include <asf.h>
+#include <board.h>
+#include <gpio.h>
+#include <sysclk.h>
+#include "busy_delay.h"
+
+// defines for USB UART
+#define CONFIG_USART_IF (AVR32_USART2)
+
+// defines for BRTT interface
+#define TEST_A AVR32_PIN_PA31
+#define RESPONSE_A AVR32_PIN_PA30
+#define TEST_B AVR32_PIN_PA29
+#define RESPONSE_B AVR32_PIN_PA28
+#define TEST_C AVR32_PIN_PA27
+#define RESPONSE_C AVR32_PIN_PB00
+
+// declare interrupts
+__attribute__((__interrupt__)) 
+static void interrupt_J3(void);
+
+void init()
+{
+	// board init
+	sysclk_init();
+	board_init();
+	busy_delay_init(BOARD_OSC0_HZ);
+	
+	// interrupts init
+	cpu_irq_disable();
+    INTC_init_interrupts();
+	INTC_register_interrupt(&interrupt_J3,   AVR32_GPIO_IRQ_3, AVR32_INTC_INT1);
+	cpu_irq_enable();
+
+	//  stdio init
+	stdio_usb_init(&CONFIG_USART_IF);
+
+	// Specify that stdout and stdin should not be buffered.
+
+#if defined(__GNUC__) && defined(__AVR32__)
+	setbuf(stdout, NULL);
+	setbuf(stdin,  NULL);
+#endif
+}
+
+/*********************************************************************
+User decelerations
+*********************************************************************/
+
+/*********************************************************************
+Interrupt routines
+*********************************************************************/
+__attribute__((__interrupt__)) 
+static void interrupt_J3(void) 
+{ 
+
+}
+
+/*********************************************************************
+Functions
+*********************************************************************/
+int main (void)
+{
+	int i;
+	// initialize
+	init();
+	
+	// start code from here
+	while(1)
+	{
+		gpio_toggle_pin(LED0_GPIO);
+		printf("tick\n");
+		
+		busy_delay_ms(500);
+	}
+}
