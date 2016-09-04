35220c98bb1d462e6c817f3bb12ed69c8c2210cf
helgeanl <helgelang@gmail.com>
2016-09-04 21:37:07 +0200

    Exercise 2

---
+++ Ex2/Ex.2-A/vfork.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<unistd.h>
+
+int global=0;
+
+int main(){
+	int i = 10;
+	pid_t pid = vfork();
+
+	if (pid == 0){
+		int j;
+		for(j=0;j<100;j++,i++,global++);
+		printf("Child process: %d, global: %d\n",i,global);
+	}else if(pid > 0){
+		int j;
+		for (j=0;j<100;j++,i++,global++);
+		printf("Parent process: %d, global: %d\n",i,global);
+	}else{
+		printf("Error\n");
+
+	}
+	return 0;
+}
