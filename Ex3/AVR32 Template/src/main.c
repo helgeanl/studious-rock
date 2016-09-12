e8d43eba50d04e6f795fe6f708a6195c7a69b847
unknown <helgeanl@ITK-D651.win.ntnu.no>
2016-09-12 12:14:28 +0200

    Exercise 3

--- Ex3/AVR32 Template/src/main.c
+++ Ex3/AVR32 Template/src/main.c
@@ -24,6 +24,10 @@
 __attribute__((__interrupt__)) 
 static void interrupt_J3(void);
 
+volatile int test_a_flag;
+volatile int test_b_flag;
+volatile int test_c_flag;
+
 void init()
 {
 	// board init
@@ -36,7 +40,7 @@ void init()
     INTC_init_interrupts();
 	INTC_register_interrupt(&interrupt_J3,   AVR32_GPIO_IRQ_3, AVR32_INTC_INT1);
 	cpu_irq_enable();
-
+	
 	//  stdio init
 	stdio_usb_init(&CONFIG_USART_IF);
 
@@ -58,7 +62,21 @@ Interrupt routines
 __attribute__((__interrupt__)) 
 static void interrupt_J3(void) 
 { 
-
+	if (gpio_get_pin_interrupt_flag(TEST_A))
+	{
+		test_a_flag = 1;
+		gpio_clear_pin_interrupt_flag(TEST_A);
+	}
+	else if (gpio_get_pin_interrupt_flag(TEST_B))
+	{
+		test_b_flag = 1;
+		gpio_clear_pin_interrupt_flag(TEST_B);
+	}
+	else if (gpio_get_pin_interrupt_flag(TEST_C))
+	{
+		test_c_flag = 1;
+		gpio_clear_pin_interrupt_flag(TEST_C);
+	}
 }
 
 /*********************************************************************
@@ -69,13 +87,49 @@ int main (void)
 	int i;
 	// initialize
 	init();
+	test_a_flag = 0;
+	test_b_flag = 0;
+	test_c_flag = 0;
+
+	gpio_configure_pin (RESPONSE_A, GPIO_DIR_OUTPUT | GPIO_INIT_HIGH);
+	gpio_configure_pin (RESPONSE_B, GPIO_DIR_OUTPUT | GPIO_INIT_HIGH);
+	gpio_configure_pin (RESPONSE_C, GPIO_DIR_OUTPUT | GPIO_INIT_HIGH);
+	gpio_enable_pin_interrupt(TEST_A,GPIO_FALLING_EDGE);
+	gpio_enable_pin_interrupt(TEST_B,GPIO_FALLING_EDGE);
+	gpio_enable_pin_interrupt(TEST_C,GPIO_FALLING_EDGE);
+	
+	gpio_set_pin_high(RESPONSE_A);
+	gpio_set_pin_high(RESPONSE_B);
+	gpio_set_pin_high(RESPONSE_C);
 	
 	// start code from here
 	while(1)
 	{
-		gpio_toggle_pin(LED0_GPIO);
-		printf("tick\n");
-		printf("tock\n");
-		busy_delay_ms(500);
+		if (test_a_flag)
+		{
+			gpio_set_pin_low(RESPONSE_A);
+			busy_delay_us(5);
+			gpio_set_pin_high(RESPONSE_A);
+			test_a_flag = 0;
+		}
+		if (test_b_flag)
+		{
+			gpio_set_pin_low(RESPONSE_B);
+			busy_delay_us(5);
+			gpio_set_pin_high(RESPONSE_B);
+			test_b_flag = 0;
+		}
+		if (test_c_flag)
+		{
+			gpio_set_pin_low(RESPONSE_C);
+			busy_delay_us(5);
+			gpio_set_pin_high(RESPONSE_C);
+			test_c_flag = 0;
+		}
+		
+		//gpio_toggle_pin(LED0_GPIO);
+		//busy_delay_ms(500);
 	}
+	
+	return 0;
 }
